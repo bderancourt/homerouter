7aa9094e9d6a6efc55fa2c8b662f1b4331a3d649
bderancourt <b2rancourt@gmail.com>
2023-02-25 00:53:40 +0100

    all in ansible

--- ansible/run_debug.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook router-playbook.yml -vvvv
---
+++ run_debug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook router-playbook.yml -vvvv
+ansible-playbook playbook.yml -vv --step
