0c804be9314a480f75971780768d632b7acdb168
Benoit DE RANCOURT <b2rancourt@gmail.com>
2025-07-30 11:25:21 +0200

    updates ikoolcore + virtualbox

--- run_debug.sh
+++ run_debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook router-playbook.yml -vvvv
-ansible-playbook playbook.yml -vv --step
+ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook install.yml -vvvv -k
+#ansible-playbook playbook.yml -vv --step
